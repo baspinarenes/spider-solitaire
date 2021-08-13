0e7e052d82869e2a673b285f51747310d9b29677
Enes Baspinar <enesbaspinar07@gmail.com>
2021-08-13 14:20:17 +0300

    Add eslint-prettier-airbnb

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React from "react";
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      Temiz
-    </div>
-  );
+  return <div className="App">Temiz</div>;
 }
 
 export default App;
